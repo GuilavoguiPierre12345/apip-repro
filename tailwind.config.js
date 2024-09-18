f63e62b221cb28be1a9b3e38153fb0a4de7e15f6
GuilavoguiPierre12345 <pierreguilao96@gmail.com>
2024-09-18 10:15:39 +0000

    update-1 responsivite

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,20 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
-      'sm': '340px',
-      'md': '540px',
-      'lg': '768px',
-      'xl': '1180px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     },
     container: {
       center : true,
